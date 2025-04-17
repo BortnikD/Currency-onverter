22ce88351848a0a2ab0a16ba57263ad0b1632ae6
dmitrii bortnik <dima100421@gmail.com>
2025-04-17 18:03:01 +0300

    feat(ConverterController): Removed extra code for error handling, now the error handler does it

--- src/main/kotlin/org/bortnik/converter/api/http/ConverterController.kt
+++ src/main/kotlin/org/bortnik/converter/api/http/ConverterController.kt
@@ -5,7 +5,6 @@ import jakarta.validation.constraints.Positive
 import jakarta.validation.constraints.Size
 import org.bortnik.converter.domain.dto.Currency
 import org.bortnik.converter.usecase.GetCurrencyDataUseCase
-import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
@@ -21,39 +20,19 @@ class ConverterController(
 
     @GetMapping
     suspend fun getCurrency(
-        @RequestParam @NotBlank @Size(min = 3, max = 3) name: String,
+        @RequestParam @NotBlank @Size(min = 2, max = 3) name: String,
         @RequestParam @Positive amount: Double = 1.0
     ): ResponseEntity<Currency> {
-
-        if(name.length > 3) {
-            ResponseEntity.status(400).body("Incorrect currency name")
-        }
-
-        return try {
-            ResponseEntity.ok(currencyDataUseCase.getCurrencyDataOfName(name, amount))
-        } catch (e: Exception) {
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
-        }
-
+            return ResponseEntity.ok(currencyDataUseCase.getCurrencyDataOfNameAndAmount(name, amount))
     }
 
     @GetMapping("/compare")
     suspend fun compareCurrencies(
-        @RequestParam @NotBlank @Size(min = 3, max = 3) from: String,
-        @RequestParam @NotBlank @Size(min = 3, max = 3) to: String,
+        @RequestParam @NotBlank @Size(min = 2, max = 3) from: String,
+        @RequestParam @NotBlank @Size(min = 2, max = 3) to: String,
         @RequestParam @Positive amount: Double = 1.0
     ): ResponseEntity<Double> {
-
-        if(from.length > 3 || to.length > 3) {
-            ResponseEntity.status(400).body("Incorrect currency name")
-        }
-
-        return try {
-            ResponseEntity.ok(currencyDataUseCase.convertCurrencies(from, to, amount))
-        } catch (e: Exception) {
-            ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build()
-        }
-
+        return ResponseEntity.ok(currencyDataUseCase.convertCurrencies(from, to, amount))
     }
 
 }
