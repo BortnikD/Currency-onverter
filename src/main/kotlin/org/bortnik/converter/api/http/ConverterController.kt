27141e5bae82f0c4948d8482db451baac29d316d
dmitrii bortnik <dima100421@gmail.com>
2025-04-18 18:54:12 +0300

    style(ConverterController): Removed TODO commentary

--- src/main/kotlin/org/bortnik/converter/api/http/ConverterController.kt
+++ src/main/kotlin/org/bortnik/converter/api/http/ConverterController.kt
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-// TODO убрать валидацию, добавить их внутрь
 @RestController
 @RequestMapping("api/converter")
 class ConverterController(
