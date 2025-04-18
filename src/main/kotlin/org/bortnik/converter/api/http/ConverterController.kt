5a3e3033b67e168474343e6c6e960875e45830ad
dmitrii bortnik <dima100421@gmail.com>
2025-04-18 15:18:21 +0300

    refactor(ConverterController): removed jakarta validation, added custom validation

--- src/main/kotlin/org/bortnik/converter/api/http/ConverterController.kt
+++ src/main/kotlin/org/bortnik/converter/api/http/ConverterController.kt
@@ -1,9 +1,7 @@
 package org.bortnik.converter.api.http
 
-import jakarta.validation.constraints.NotBlank
-import jakarta.validation.constraints.Positive
-import jakarta.validation.constraints.Size
 import org.bortnik.converter.domain.dto.Currency
+import org.bortnik.converter.domain.exceptions.InvalidRequestException
 import org.bortnik.converter.usecase.GetCurrencyDataUseCase
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
@@ -11,28 +9,41 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
-
+// TODO убрать валидацию, добавить их внутрь
 @RestController
 @RequestMapping("api/converter")
 class ConverterController(
-    private val currencyDataUseCase: GetCurrencyDataUseCase
+    private val currencyUseCase: GetCurrencyDataUseCase
 ) {
 
     @GetMapping
     suspend fun getCurrency(
-        @RequestParam @NotBlank @Size(min = 2, max = 3) name: String,
-        @RequestParam @Positive amount: Double = 1.0
+        @RequestParam name: String,
+        @RequestParam amount: Double = 1.0
     ): ResponseEntity<Currency> {
-            return ResponseEntity.ok(currencyDataUseCase.getCurrencyDataOfNameAndAmount(name, amount))
+        if (name.length != 3) {
+           throw InvalidRequestException("The name length must be equal to 3")
+        }
+        if (amount <= 0) {
+            throw InvalidRequestException("The amount must be greet then 0")
+        }
+        return ResponseEntity.ok(currencyUseCase.getCurrencyDataOfNameAndAmount(name, amount))
     }
 
     @GetMapping("/compare")
     suspend fun compareCurrencies(
-        @RequestParam @NotBlank @Size(min = 2, max = 3) from: String,
-        @RequestParam @NotBlank @Size(min = 2, max = 3) to: String,
-        @RequestParam @Positive amount: Double = 1.0
+        @RequestParam from: String,
+        @RequestParam to: String,
+        @RequestParam amount: Double = 1.0
     ): ResponseEntity<Double> {
-        return ResponseEntity.ok(currencyDataUseCase.convertCurrencies(from, to, amount))
+        if (from.length != 3 || to.length != 3) {
+            throw InvalidRequestException("The name length must be equal to 3")
+        }
+        if (amount <= 0) {
+            throw InvalidRequestException("The amount must be greet then 0")
+        }
+
+        return ResponseEntity.ok(currencyUseCase.convertCurrencies(from, to, amount))
     }
 
 }
