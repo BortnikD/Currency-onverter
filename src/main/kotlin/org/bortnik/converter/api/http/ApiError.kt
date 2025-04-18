257c7618192d9d0766dd12d721be8c3062079df3
dmitrii bortnik <dima100421@gmail.com>
2025-04-18 16:25:48 +0300

    refactor(ExceptionsHandler): added more details to ApiError, created response entities builder

--- src/main/kotlin/org/bortnik/converter/api/http/ApiError.kt
+++ src/main/kotlin/org/bortnik/converter/api/http/ApiError.kt
@@ -1,8 +1,11 @@
 package org.bortnik.converter.api.http
 
+import org.springframework.http.HttpStatus
 import java.time.LocalDateTime
 
 data class ApiError(
+    val error: String,
     val message: String,
+    val status: HttpStatus,
     val timestamp: LocalDateTime = LocalDateTime.now()
 )
--- src/main/kotlin/org/bortnik/converter/api/http/ExceptionsHandler.kt
+++ src/main/kotlin/org/bortnik/converter/api/http/ExceptionsHandler.kt
@@ -3,6 +3,7 @@ package org.bortnik.converter.api.http
 import org.bortnik.converter.domain.exceptions.CurrencyApiException
 import org.bortnik.converter.domain.exceptions.CurrencyNotFoundException
 import org.bortnik.converter.domain.exceptions.InvalidRequestException
+import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
 import org.springframework.web.bind.annotation.RestControllerAdvice
@@ -13,47 +14,60 @@ class ExceptionsHandler {
 
     @ExceptionHandler(CurrencyNotFoundException::class)
     fun handleCurrencyNotFound(e: CurrencyNotFoundException): ResponseEntity<ApiError> {
-        return ResponseEntity
-            .status(404)
-            .body(
-                ApiError(message = e.message ?: "Currency not found")
+        return buildResponseEntity(
+            ApiError(
+                error = "Currency Not Found",
+                message = e.message ?: "Currency not found",
+                status = HttpStatus.NOT_FOUND
             )
+        )
     }
 
     @ExceptionHandler(InvalidRequestException::class)
     fun handleInvalidRequestException(e: InvalidRequestException): ResponseEntity<ApiError> {
-        return ResponseEntity
-            .status(400)
-            .body(
-                ApiError(message = e.message ?: "Invalid request parameters")
+        return buildResponseEntity(
+            ApiError(
+                error = "Invalid Request",
+                message = e.message ?: "Invalid request parameters",
+                status = HttpStatus.BAD_REQUEST
             )
+        )
     }
 
     @ExceptionHandler(CurrencyApiException::class)
     fun handleCurrencyApiException(e: CurrencyApiException): ResponseEntity<ApiError> {
-        return ResponseEntity
-            .status(500)
-            .body(
-                ApiError(message = e.message ?: "Internal Server Error")
+        return buildResponseEntity(
+            ApiError(
+                error = "Currency Api Exception",
+                message = e.message ?: "Internal Server Error",
+                status = HttpStatus.INTERNAL_SERVER_ERROR
             )
+        )
     }
 
     @ExceptionHandler(MissingRequestValueException::class)
     fun handleMissingRequestValueException(e: MissingRequestValueException): ResponseEntity<ApiError> {
-        return ResponseEntity
-            .status(400)
-            .body(
-                ApiError("Missing request parameter")
+        return buildResponseEntity(
+            ApiError(
+                error = "Missing Request Value",
+                message = e.message,
+                status = HttpStatus.BAD_REQUEST
             )
+        )
     }
 
     @ExceptionHandler(Exception::class)
     fun handleGenericException(e: Exception): ResponseEntity<ApiError> {
-        return ResponseEntity
-            .status(500)
-            .body(ApiError(
-                message = "Internal server error"
-            ))
+        return buildResponseEntity(
+            ApiError(
+                error = "Internal server error",
+                message = e.message ?: "An unexpected error occurred",
+                status = HttpStatus.INTERNAL_SERVER_ERROR
+            )
+        )
     }
 
+    fun buildResponseEntity(apiError: ApiError): ResponseEntity<ApiError> {
+        return ResponseEntity(apiError, apiError.status)
+    }
 }
