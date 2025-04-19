a5c67cb80b2b125ff074fa0afb80047c5c471b5e
dmitrii bortnik <dima100421@gmail.com>
2025-04-19 16:59:48 +0300

    feat(GetCurrencyDataUseCase): Added redis caching

--- src/main/kotlin/org/bortnik/converter/usecase/GetCurrencyDataUseCase.kt
+++ src/main/kotlin/org/bortnik/converter/usecase/GetCurrencyDataUseCase.kt
@@ -2,6 +2,7 @@ package org.bortnik.converter.usecase
 
 import org.bortnik.converter.domain.dto.Currency
 import org.bortnik.converter.domain.repositories.CurrencyRepository
+import org.springframework.cache.annotation.Cacheable
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,10 +10,12 @@ class GetCurrencyDataUseCase(
     private val currencyRepository: CurrencyRepository
 ) {
 
+    @Cacheable(value = ["Currency"], key = "#name + '_' + #amount")
     suspend fun getCurrencyDataOfNameAndAmount(name: String, amount: Double): Currency {
         return currencyRepository.getCurrencyDataOfNameAndAmount(name, amount)
     }
 
+    @Cacheable(value = ["CurrencyConverted"], key = "#from + '_to_' + #to + '_amount_' + #amount")
     suspend fun convertCurrencies(from: String, to: String, amount: Double = 1.0): Double {
         val response = currencyRepository.getCurrencyDataOfNameAndAmount(from, amount)
         return response.rates[to]!!
