56e509689ffd7907a176a4823f20d1caa62bbc3d
dmitrii bortnik <dima100421@gmail.com>
2025-04-23 18:55:24 +0300

    feat(usecase): created GetExchangeCurrencyUseCase

---
+++ src/main/kotlin/org/bortnik/converter/usecase/GetExchangeCurrencyUseCase.kt
@@ -0,0 +1,24 @@
+package org.bortnik.converter.usecase
+
+import org.bortnik.converter.domain.dto.ExchangeSessionWithRates
+import org.bortnik.converter.domain.dto.exchangeSession.toDto
+import org.bortnik.converter.domain.dto.exchangeRate.toDto
+import org.bortnik.converter.domain.repositories.ExchangeSessionRepository
+import org.bortnik.converter.infrastructure.repositories.ExchangeRatesRepository
+
+
+class GetExchangeCurrencyUseCase (
+    private val sessionRepository: ExchangeSessionRepository,
+    private val ratesRepository: ExchangeRatesRepository
+) {
+
+    fun getExchangeCurrency(currency: String): ExchangeSessionWithRates {
+        val session = sessionRepository.findByBaseCurrencyOrderByDateDesc(currency) ?: TODO()
+        val sessionRates = ratesRepository.findBySessionId(session.id!!) ?: TODO()
+        return ExchangeSessionWithRates(
+            session.toDto(),
+            sessionRates.map { it.toDto() }
+        )
+    }
+
+}
