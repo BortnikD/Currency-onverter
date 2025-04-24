415ab3242100eabd83bfde544e64aedfcbc58592
dmitrii bortnik <dima100421@gmail.com>
2025-04-24 16:24:10 +0300

    refactor(usecase): renamed getExchangeCurrency to findByBaseCurrencyOrderByDateDesc

--- src/main/kotlin/org/bortnik/converter/usecase/GetExchangeCurrencyUseCase.kt
+++ src/main/kotlin/org/bortnik/converter/usecase/GetExchangeCurrencyUseCase.kt
@@ -3,19 +3,19 @@ package org.bortnik.converter.usecase
 import org.bortnik.converter.domain.dto.ExchangeSessionWithRates
 import org.bortnik.converter.domain.repositories.ExchangeSessionRepository
 import org.bortnik.converter.domain.repositories.ExchangeRateRepository
+import org.springframework.stereotype.Service
 
-// TODO добавить корректную обработку ошибок, вместо TODO()
 // TODO добавить кэширование результатов
-// TODO написать контроллер для данного кейса
 
+@Service
 class GetExchangeCurrencyUseCase (
     private val sessionRepository: ExchangeSessionRepository,
     private val ratesRepository: ExchangeRateRepository
 ) {
 
-    suspend fun getExchangeCurrency(currency: String): ExchangeSessionWithRates {
-        val session = sessionRepository.findByBaseCurrencyOrderByDateDesc(currency) ?: TODO()
-        val sessionRates = ratesRepository.findBySessionId(session.id!!) ?: TODO()
+    suspend fun findByBaseCurrencyOrderByDateDesc(currency: String): ExchangeSessionWithRates {
+        val session = sessionRepository.findByBaseCurrencyOrderByDateDesc(currency)
+        val sessionRates = ratesRepository.findBySessionId(session.id!!)
         return ExchangeSessionWithRates(
             session,
             sessionRates
