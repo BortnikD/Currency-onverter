2f48d622e890e8348aeb782b698576cf1d8647f8
dmitrii bortnik <dima100421@gmail.com>
2025-04-24 14:33:45 +0300

    feat(ExceptionsHandler): added new exceptions handling

--- src/main/kotlin/org/bortnik/converter/api/http/ExceptionsHandler.kt
+++ src/main/kotlin/org/bortnik/converter/api/http/ExceptionsHandler.kt
@@ -3,6 +3,10 @@ package org.bortnik.converter.api.http
 import org.bortnik.converter.domain.exceptions.CurrencyApiException
 import org.bortnik.converter.domain.exceptions.CurrencyNotFoundException
 import org.bortnik.converter.domain.exceptions.InvalidRequestException
+import org.bortnik.converter.domain.exceptions.rate.RateCreateError
+import org.bortnik.converter.domain.exceptions.rate.RateNotFound
+import org.bortnik.converter.domain.exceptions.session.SessionCreateError
+import org.bortnik.converter.domain.exceptions.session.SessionNotFound
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.ExceptionHandler
@@ -41,6 +45,50 @@ class ExceptionsHandler {
                 error = "Currency Api Exception",
                 message = e.message ?: "Internal Server Error",
                 status = HttpStatus.INTERNAL_SERVER_ERROR
+            )
+        )
+    }
+
+    @ExceptionHandler(RateNotFound::class)
+    fun handleRateNotFound(e: RateNotFound): ResponseEntity<ApiError> {
+        return buildResponseEntity(
+            ApiError(
+                error = "Rate not found",
+                message = e.message ?: "rate not found",
+                status = HttpStatus.NOT_FOUND
+            )
+        )
+    }
+
+    @ExceptionHandler(SessionNotFound::class)
+    fun handleRSessionNotFound(e: SessionNotFound): ResponseEntity<ApiError> {
+        return buildResponseEntity(
+            ApiError(
+                error = "Session Not Found",
+                message = e.message ?: "Session Not Found",
+                status = HttpStatus.NOT_FOUND
+            )
+        )
+    }
+
+    @ExceptionHandler(RateCreateError::class)
+    fun handleRateCreateError(e: RateCreateError): ResponseEntity<ApiError> {
+        return buildResponseEntity(
+            ApiError(
+                error = "Rate Create Error",
+                message = e.message ?: "Rate Create Error",
+                status = HttpStatus.BAD_REQUEST
+            )
+        )
+    }
+
+    @ExceptionHandler(SessionCreateError::class)
+    fun handleSessionCreateError(e: SessionCreateError): ResponseEntity<ApiError> {
+        return buildResponseEntity(
+            ApiError(
+                error = "Session Create Error",
+                message = e.message ?: "Session Create Error",
+                status = HttpStatus.BAD_REQUEST
             )
         )
     }
