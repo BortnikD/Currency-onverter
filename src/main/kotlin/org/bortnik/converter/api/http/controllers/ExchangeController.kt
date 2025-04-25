6c42a99f74d2324b6442bca6f82bf2b65f4d5ae8
dmitrii bortnik <dima100421@gmail.com>
2025-04-25 11:03:45 +0300

    feat(controllers): added new endpoint to create sessionWithRates

--- src/main/kotlin/org/bortnik/converter/api/http/controllers/ExchangeController.kt
+++ src/main/kotlin/org/bortnik/converter/api/http/controllers/ExchangeController.kt
@@ -2,16 +2,20 @@ package org.bortnik.converter.api.http.controllers
 
 import org.bortnik.converter.domain.dto.ExchangeSessionWithRates
 import org.bortnik.converter.domain.exceptions.currency.InvalidRequestException
+import org.bortnik.converter.usecase.CreateExchangeCurrencyUseCase
 import org.bortnik.converter.usecase.GetExchangeCurrencyUseCase
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api/exchange")
 class ExchangeController(
-    private val getExchangeUseCase: GetExchangeCurrencyUseCase
+    private val getExchangeUseCase: GetExchangeCurrencyUseCase,
+    private val createExchangeCurrencyUseCase: CreateExchangeCurrencyUseCase
 ) {
 
     @GetMapping("/{currencyName}")
@@ -22,6 +26,13 @@ class ExchangeController(
             throw InvalidRequestException("length currency name must be equal 3")
         }
         return getExchangeUseCase.findByBaseCurrencyOrderByDateDesc(currencyName.uppercase())
+    }
+
+    @PostMapping
+    suspend fun setExchangeSessionWithRates(
+        @RequestBody sessionWithRates: ExchangeSessionWithRates
+    ): ExchangeSessionWithRates {
+        return createExchangeCurrencyUseCase.saveSessionWithRates(sessionWithRates)
     }
 
 }
